b3058849149bf14d41bf47ee9fb455224cf74586
dj.jeong <sam1087@mju.ac.kr>
2025-10-02 00:07:14 +0900

    잘못된 theme 파일 교체

--- src/main/java/dev/mju/jobport/domain/sample/controller/SampleController.java
+++ src/main/java/dev/mju/jobport/domain/sample/controller/SampleController.java
@@ -1,14 +1,20 @@
 package dev.mju.jobport.domain.sample.controller;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@Slf4j
 @Controller
-@RequestMapping("samples")
+@RequestMapping("/")
 public class SampleController {
-    @GetMapping
-    public String index() {
-        return "samples/index";
+    @GetMapping(value = "/{path}.html", produces = MediaType.TEXT_HTML_VALUE)
+    public String proxyHtml(@PathVariable String path) {
+
+        log.info("path={}", path.split("/")[path.split("/").length - 1]);
+        return "samples/" + path.split("/")[path.split("/").length - 1] + ".html";
     }
 }
